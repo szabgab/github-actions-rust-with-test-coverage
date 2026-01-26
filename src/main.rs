3c7a55ffe95914f61fc88b57f3934e9cd06dd6b7
Gabor Szabo <gabor@szabgab.com>
2026-01-26 19:09:29 +0200

    more code

--- src/main.rs
+++ src/main.rs
@@ -7,12 +7,21 @@ fn add(a: u32, b: u32) -> u32 {
     a + b
 }
 
+#[allow(dead_code)]
+fn multiply(a: u32, b: u32) -> u32 {
+    a * b
+}
+
 #[cfg(test)]
 mod test {
     use super::*;
 
     #[test]
-    fn it_works() {
+    fn add_works() {
         assert_eq!(add(2, 3), 5);
     }
+    // #[test]
+    // fn multiply_works() {
+    //     assert_eq!(multiply(2, 3), 6);
+    // }
 }
